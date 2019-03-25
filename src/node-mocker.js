e299faedd72ad59d2bc9f6775dc9d5c302b8a8ae
Sonjoo <joohotheman@gmail.com>
2019-03-25 14:40:46 +0900

    mocker.js src change var mock = fn to var mock = {...}

--- src/node-mocker.js
+++ src/node-mocker.js
@@ -1,83 +1,89 @@
 //with mock functions
 var mocker = (function() {
-  var mock = {};
-  mock.comp = function(a) {
-    return b => b ? true : false;
-  }
+  var mock = {
+    comp: function(a) {
+      return b => b ? true : false;
+    },
 
-  mock.getFuntion = function(fn_target) {
-    var fn = fn_target;
-  }
+    getFuntion: function(fn_target) {
+      var fn = fn_target;
+    },
 
-  mock.objModifier = function(obj) {
-    return prop => function(val) {
-      obj[prop] = val;
-    };
-  }
-
-  mock.db = function() {
-    var db = {};
-    db.createTable = function(table_obj) {
-      db[table_obj.table_name] = table_obj;
-    };
-
-    db.addMember = function(table_name, entity) {
-      db[table_name][entity.id] = entity;
-    };
-
-    db.deleteMember = function(table_name, entity_id) {
-      delete db[table_name][entity_id];
-    }
-
-    db.returnMember = function(table_name, entity_id) {
-      return db[table_name][entity_id];
-    }
-
-    db.updateMember = function(table_name, entity) {
-      for (var val in entity) {
-        db[table_name][entity.id][val] = entity[val];
+    objModifier: function(obj) {
+      var mock_obj = {};
+      for (var prop in obj) {
+        mock_obj[prop] = obj[prop];
       }
-    }
-    return db;
-  }();
+      return mock_obj;
+    },
 
-  mock.objectParser = function(obj) {
-    let type_list = [];
-    for (var prop in obj) {
-      if (typeof prop === 'object' && toString.call(obj) !== '[object Array]') {
-        type_list.push('object');
+    db: (function() {
+      var db = {
+        createTable: function(table_obj) {
+          this[table_obj.table_name] = table_obj;
+        },
+
+        addMember: function(table_name, entity) {
+          this[table_name][entity.id] = entity;
+        },
+
+        deleteMember: function(table_name, entity_id) {
+          delete this[table_name][entity_id];
+        },
+
+        returnMember: function(table_name, entity_id) {
+          return this[table_name][entity_id];
+        },
+
+        updateMember: function(table_name, entity) {
+          for (var val in entity) {
+            this[table_name][entity.id][val] = entity[val];
+          }
+        },
       }
-      else if (toString.call(obj) === '[object Array]') {
-        type_list.push('Array');
+      return db;
+    })(),
+
+    objectParser: function(obj) {
+      let type_list = [];
+      for (var prop in obj) {
+        if (typeof prop === 'object' && toString.call(obj) !== '[object Array]') {
+          type_list.push('object');
+        }
+        else if (toString.call(obj) === '[object Array]') {
+          type_list.push('Array');
+        }
       }
-    }
-  }
+    },
+    
+    mockInput: {
+      num: function() {
+        return Math.random();
+      },
 
-  mock.randomNum = function() {
-    return Math.random();
-  }
+      int: function() {
+        return Math.floor(this.randomNum());
+      },
 
-  mock.randomInt = function() {
-    return Math.floor(this.randomNum());
-  }
+      signeNum: function() {
+        var num = this.randomNum();
+        return num < 0 ? -1 * num : num;
+      },
 
-  mock.randomSigneNum = function() {
-    var num = this.randomNum();
-    return num < 0 ? -1 * num : num;
-  }
+      signInt: function() {
+        var num = this.randomInt();
+        return Math.floor(num < 0 ? -1 * num : num);
+      },
 
-  mock.randomSignInt = function() {
-    var num = this.randomInt();
-    return Math.floor(num < 0 ? -1 * num : num);
-  }
+      stringInput: function(size) {
+        var str = '';
+        for (let i = 0; i < len; i++) {
+          str += String.fromCharCode(this.randomSignInt());
+        }
+        return str;
+      }
+    },
 
-  mock.randomStringInput = function(size) {
-    var str = '';
-    for (let i = 0; i < len; i++) {
-      str += String.fromCharCode(this.randomSignInt());
-    }
-    return str;
   }
-
   return mock;
 }());
